56e87e64c7190fb5ad58714c27712143d2e18626
Oh-Kang94 <okh19941994@naver.com>
2025-09-07 18:39:18 +0900

    Router 하나로 묶어서 처리하기

--- main.go
+++ main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"web-basic/src"
 )
 
 func main() {
-	// "/"경로로 접속했을때 처리할 핸들러 처리
-	http.HandleFunc("/", printHelloWorld)
 
-	http.ListenAndServe(":8000", nil)
-}
+	r := &src.Router{
+		Handlers: make(map[string]map[string]http.HandlerFunc),
+	}
 
-func printHelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(fmt.Fprintln(w, "welcome!"))
+	r.HandleFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "Welcome!")
+	})
+
+	r.HandleFunc("GET", "/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "about!")
+	})
+	r.HandleFunc("GET", "/users/:id", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "user id: \n")
+	})
+	r.HandleFunc("GET", "/users/:user_id/address/:address_id", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Get User Id: \nGet Address: \n")
+	})
+
+	r.HandleFunc("POST", "/users/:user_id/addresses", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "CREATE USER")
+	})
+
+	http.ListenAndServe(":8000", r)
 }
--- src/router.go
+++ src/router.go
@@ -2,34 +2,65 @@ package src
 
 import (
 	"net/http"
+	"strings"
 )
 
-type router struct {
-	handlers map[string]map[string]func(http.ResponseWriter, *http.Request) // http.HandlerFunc Mapping
+type Router struct {
+	Handlers map[string]map[string]http.HandlerFunc // http.HandlerFunc Mapping
 }
 
 type Handler interface {
 	ServeHttp(http.ResponseWriter, *http.Request)
 }
 
-func (r *router) HandleFunc(method, pattern string, h http.HandlerFunc) {
-	m, ok := r.handlers[method]
+func (r *Router) HandleFunc(method, pattern string, h http.HandlerFunc) {
+	m, ok := r.Handlers[method]
 	if !ok {
 		// 등록된 Map이 없으면 생성
-		m = make(map[string]func(http.ResponseWriter, *http.Request))
-		r.handlers[method] = m
+		m = make(map[string]http.HandlerFunc)
+		r.Handlers[method] = m
 
 	}
 
 	m[pattern] = h
 }
 
-func (r *router) ServeHttp(w http.ResponseWriter, req *http.Request) {
-	if m, ok := r.handlers[req.Method]; ok {
-		if h, ok := m[req.URL.Path]; ok {
-			h(w, req)
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	for pattern, handler := range r.Handlers[req.Method] {
+		if ok, _ := match(pattern, req.URL.Path); ok {
+			handler(w, req)
 			return
 		}
 	}
+
 	http.NotFound(w, req)
+}
+
+func match(pattern, path string) (bool, map[string]string) {
+	// 패턴과 Path일치하면 true
+	if pattern == path {
+		return true, nil
+	}
+
+	// Pattern , path를 '/'로 구분
+	patterns := strings.Split(pattern, "/")
+	paths := strings.Split(path, "/")
+
+	if len(patterns) != len(paths) {
+		return false, nil
+	}
+
+	params := make(map[string]string)
+	for i := range patterns {
+		switch {
+		case patterns[i] == paths[i]:
+		case len(patterns[i]) > 0 && patterns[i][0] == ':':
+			params[patterns[i][1:]] = paths[i]
+		default:
+			return false, nil
+		}
+
+	}
+
+	return true, params
 }
